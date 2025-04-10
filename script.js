b8aa2665fd4511b74179cf91b6abe5da8b733a92
AlexTheGreay <73349977+AlexTheGreay@users.noreply.github.com>
2025-04-10 12:12:26 -0400

    Worked on reflections file

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ function toggleDarkMode() {
     localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
 }
 
+// Collapse other details when one is opened
+document.querySelectorAll('details').forEach((detail) => {
+    detail.addEventListener('toggle', function () {
+        if (this.open) {
+            document.querySelectorAll('details').forEach((other) => {
+                if (other !== this) other.removeAttribute('open');
+            });
+        }
+    });
+});
+
+
 // Load user's preference
 if (localStorage.getItem('darkMode') === 'true') {
     document.body.classList.add('dark-mode');
